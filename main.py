1483ebbb5f77e08e5224633165b7a518df2ed557
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2023-02-11 12:45:24 +0100

    joining sets, accessing sets, adding sets

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def my_set():
+    this_set = {"apple", "banana", "cherry", "mango"}
+    return this_set
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+tropical = {"pineapple", "mango", "papaya"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+mylist = ["kiwi", "orange"]
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+mySet = my_set()
+print(mySet)
+
+print(len(mySet))
+
+# Access set items
+for x in mySet:
+    print(x)
+
+print()
+
+print("banana" in mySet)
+
+# Adding to a set
+mySet.add("orange")
+print(mySet)
+
+# Adding to a set
+mySet.update(tropical)
+print(mySet)
+
+print()
+
+mySet.update(mylist)  # Adding an iterable eg a list to a set
+print(mySet)
+print()
+
+# Removing from a set
+mySet.remove("banana")
+print(mySet)
+
+mySet.remove("kiwi")
+print(mySet)
+
+x = mySet.pop()
+print(x)
+print(mySet)
+print()
+
+# Looping through a set
+for x in mySet:
+    print(x)
+
+print()
+
+# Joining sets
+set3 = set1.union(set2)
+print(set3)
+
+print()
+
+set1.update(set2)
+print(set1)
+
+print()
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)  # Keep the items that exist in both set x, and set y
+print(x)
+
+z = x.intersection(y)  # returns a new set, that only contains the items that are present in both sets
+print(z)
+
+# x.symmetric_difference_update(y)  # will keep only the elements that are NOT present in both sets
+# print(x)
+
+z = x.symmetric_difference(y)  # returns a new set, that contains only the elements that are NOT
+print(z)
+
+
